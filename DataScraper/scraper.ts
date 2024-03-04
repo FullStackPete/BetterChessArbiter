adbce7d5149d1c32f450867c0c7c7a7a455e9a32
FullStackPete <psliwa.b@gmail.com>
2024-03-04 22:03:10 +0100

    Scraper update

--- DataScraper/scraper.ts
+++ DataScraper/scraper.ts
@@ -4,64 +4,84 @@ import path from "path";
 const url = "https://www.chessarbiter.com/";
 
 type urlsType = {
-  aTag:string
-}
+  aTag: string;
+};
 
-let eventPages: string[] = [];
 function delay(time: number) {
   return new Promise(function (resolve) {
     setTimeout(resolve, time);
   });
 }
-const ScrapeEventPage = async (urls:urlsType[], browser:Browser) => {
+const ScrapeEventPage = async (urls: urlsType[], browser: Browser) => {
   if (!urls) return console.log("No urls passed as a parameter.");
-
+  const commonData = [];
   const page = await browser.newPage();
-  
-  for (let i=0;i<urls.length;i++){
-    await page.goto(urls[i].aTag);
-    
 
+  for (let i = 0; i < urls.length; i++) {
+    await page.goto(urls[i].aTag);
+
+    const dataScrape = await page.evaluate(() => {
+      const tables: NodeListOf<HTMLTableElement> =
+        document.querySelectorAll(".panel-table");
+      return Array.from(tables).filter((table)=>table.innerText.includes("Informacje podstawowe")).flatMap((table) => {
+        if (table.innerText.includes("Informacje podstawowe")) {
+          const commonInfoTable = table.querySelectorAll("table");
+          return Array.from(commonInfoTable)
+            .slice(1)
+            .flatMap((table) => {
+              const commonInfo = table?.querySelectorAll("tr");
+              const title = commonInfo[0].querySelectorAll("td")[1].innerText;
+              const startDate =
+                commonInfo[1].querySelectorAll("td")[1].innerText;
+              const endDate = commonInfo[2].querySelectorAll("td")[1].innerText;
+              const place = commonInfo[3].querySelectorAll("td")[1].innerText;
+              const gameTempo =
+                commonInfo[4].querySelectorAll("td")[1].innerText;
+              const referee = commonInfo[5].querySelectorAll("td")[1].innerText;
+              const organizer =
+                commonInfo[6].querySelectorAll("td")[1].innerText;
+              const roundsTotal =
+                commonInfo[8].querySelectorAll("td")[1].innerText;
+              const roundsEnded =
+                commonInfo[7].querySelectorAll("td")[1].innerText;
+              const system = commonInfo[9].querySelectorAll("td")[1].innerText;
+              if (commonInfo) {
+                return {
+                  title,
+                  startDate,
+                  endDate,
+                  place,
+                  gameTempo,
+                  referee,
+                  organizer,
+                  roundsTotal,
+                  roundsEnded,
+                  system,
+                };
+              } else return;
+            });
+        } else {
+          return;
+        }
+      });
+    });    
+      commonData.push(dataScrape);
   }
 
-  const dataScrape = await page.evaluate(() => {
-    const tables: NodeListOf<HTMLTableElement> =
-      document.querySelectorAll(".panel-table");
-    return Array.from(tables).map((table) => {
-      if (table.innerText.includes("Informacje podstawowe")) {
-        const commonInfoTable = table.querySelector("table");
-        const commonInfo = commonInfoTable!.querySelectorAll("tr");
-
-        const startDate = commonInfo[0].querySelectorAll("td")[1].innerText;
-        const endDate = commonInfo[1].querySelectorAll("td")[1].innerText;
-        const place = commonInfo[2].querySelectorAll("td")[1].innerText;
-        const gameTempo = commonInfo[3].querySelectorAll("td")[1].innerText;
-        const referee = commonInfo[4].querySelectorAll("td")[1].innerText;
-        const organizer = commonInfo[5].querySelectorAll("td")[1].innerText;
-        const roundsTotal = commonInfo[6].querySelectorAll("td")[1].innerText;
-        const roundsEnded = commonInfo[7].querySelectorAll("td")[1].innerText;
-        const system = commonInfo[8].querySelectorAll("td")[1].innerText;
-        return {
-          startDate,
-          endDate,
-          place,
-          gameTempo,
-          referee,
-          organizer,
-          roundsTotal,
-          roundsEnded,
-          system,
-        };
-      }
-    });
+  
+  const flatCommonData = commonData.flat();
+  const currentPath = path.resolve(".");
+  fs.writeFile(currentPath + "/details.txt", JSON.stringify(flatCommonData), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("File written success");
+    }
   });
-  await page.close();
-  console.log(dataScrape);
-  return dataScrape;
+  await browser.close();
 };
 
 const ScrapeFrontPage = async () => {
-  let commonInfo = undefined;
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto(url);
@@ -113,7 +133,7 @@ const ScrapeFrontPage = async () => {
       });
   });
   const currentPath = path.resolve(".");
-  fs.writeFile(currentPath + "/baza.txt", JSON.stringify(dataScrape), (err) => {
+  fs.writeFile(currentPath + "/db.txt", JSON.stringify(dataScrape), (err) => {
     if (err) {
       console.log(err);
     } else {
@@ -123,7 +143,7 @@ const ScrapeFrontPage = async () => {
   await browser.close();
 };
 
-const ScrapeLinks = async (browser:Browser) => {  
+const ScrapeLinks = async (browser: Browser) => {
   const page = await browser.newPage();
   await page.goto(url);
 
@@ -140,16 +160,14 @@ const ScrapeLinks = async (browser:Browser) => {
             return { aTag };
           });
       });
-    
   });
-  browser.close();
-  return dataScrape;
-  
-};
-const ScrapingEventPages = async ()=>{
-  const browser = puppeteer.launch();
-  const linksArray = await ScrapeLinks(await browser);
-  await ScrapeEventPage(linksArray, await browser);
 
-  
-} 
+  return dataScrape;
+};
+const ScrapingEventPages = async () => {
+  const browser = await puppeteer.launch({ headless: false });  
+  const linksArray = await ScrapeLinks(browser);
+  await ScrapeEventPage(linksArray, browser);
+
+};
+ScrapingEventPages();
